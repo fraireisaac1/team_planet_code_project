10d13dea85b2abca110c65db73bc1074cfa2f0a2
octaviomcnaughton@gmail.com <octaviomcnaughton@gmail.com>
2024-08-16 17:09:34 -0700

    bug fixes

--- starter/finalCode.js
+++ starter/finalCode.js
@@ -3,18 +3,20 @@ const prompt = require('prompt-sync')();
 // Import the gravityFactors module which contains factors for different planets
 let gravityFactors = require('./utils/earthGravityFactors.js');
 let alienGravityFactors = require('./utils/alienGravityFactors.js');
+let factorGalaxy;
 // Define a function to show user factors based on input type and value
 function showUserFactors(factorType, factorSystem, factorValue, factorGalaxy) {
     // Initialize an object to hold the results
     const factors = {};
     // Declare a variable to hold the unit of measurement
     // Iterate over each item in the gravityFactors object
-    for (let planet in gravityFactors) {
+    let galaxy = (factorGalaxy == 1) ? gravityFactors : alienGravityFactors;
+    for (let planet in galaxy) {
         // Calculate the factor multiplied by the input value and round
         if (factorType == 1) {
-            factors[planet] = parseFloat((factorValue / gravityFactors[planet]).toFixed(2));
+            factors[planet] = parseFloat((factorValue / galaxy[planet]).toFixed(2));
         } else {
-            factors[planet] = parseFloat((factorValue * gravityFactors[planet]).toFixed(2));
+            factors[planet] = parseFloat((factorValue * galaxy[planet]).toFixed(2));
         }
     }
     // Switch case to determine the measurement unit based on factor type
@@ -41,7 +43,6 @@ function getUserInput() {
     let factorType;
     let factorSystem;
     let factorValue;
-    let factorGalaxy;
     // Call the showUserFactors function with the user inputs and the gravity
     while (true) {
         console.log(`Enter what type you want to measure (enter "1" for jump, "2" for weight, or "3" for pushups)...`);
@@ -66,7 +67,7 @@ function getUserInput() {
     }
     while (true) {
         console.log(`Enter which galaxy you want to measure in (enter "1" for the milky way, or enter "2" for andromeda)...`);
-        factorValue = prompt(`>> `);
+        factorGalaxy = prompt(`>> `);
         if (parseFloat(factorGalaxy) > 2 || parseFloat(factorGalaxy) < 1) {
             console.log(`You made a mistake.`);
         } else { break; }
