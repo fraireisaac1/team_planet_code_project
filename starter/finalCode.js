676420e530460404d52cd3aa5e9ed074c944df30
Isaac Fraire <ifrair729@west-mec.org>
2024-08-16 17:05:38 -0700

    updated commit

--- starter/finalCode.js
+++ starter/finalCode.js
@@ -2,15 +2,20 @@
 const prompt = require('prompt-sync')();
 // Import the gravityFactors module which contains factors for different planets
 let gravityFactors = require('./utils/earthGravityFactors.js');
+let alienGravityFactors = require('./utils/alienGravityFactors.js');
 // Define a function to show user factors based on input type and value
-function showUserFactors(factorType, factorSystem, factorMeasurement, factorValue) {
+function showUserFactors(factorType, factorSystem, factorValue, factorGalaxy) {
     // Initialize an object to hold the results
     const factors = {};
     // Declare a variable to hold the unit of measurement
     // Iterate over each item in the gravityFactors object
     for (let planet in gravityFactors) {
         // Calculate the factor multiplied by the input value and round
-        factors[planet] = parseFloat((factorValue * gravityFactors[planet]).toFixed(2));
+        if (factorType == 1) {
+            factors[planet] = parseFloat((factorValue / gravityFactors[planet]).toFixed(2));
+        } else {
+            factors[planet] = parseFloat((factorValue * gravityFactors[planet]).toFixed(2));
+        }
     }
     // Switch case to determine the measurement unit based on factor type
     const factorTypes = [undefined, "jump", "weight", "pushups"];
@@ -35,38 +40,38 @@ function getUserInput() {
     // Prompt the user to enter the numerical value
     let factorType;
     let factorSystem;
-    let factorMeasurement;
     let factorValue;
+    let factorGalaxy;
     // Call the showUserFactors function with the user inputs and the gravity
     while (true) {
         console.log(`Enter what type you want to measure (enter "1" for jump, "2" for weight, or "3" for pushups)...`);
         factorType = prompt(`>> `);
-        if (parseInt(factorType) > 3 || parseInt(factorType) < 1) {
+        if (parseFloat(factorType) > 3 || parseFloat(factorType) < 1) {
             console.log(`You made a mistake.`);
         } else { break; }
     }
     while (true) {
         console.log(`Enter what type of system you want to measure in (enter "1" for metric, or "2" for imperial)...`);
         factorSystem = prompt(`>> `);
-        if (parseInt(factorSystem) > 2 || parseInt(factorSystem) < 1) {
-            console.log(`You made a mistake.`);
-        } else { break; }
-    }
-    while (true) {
-        console.log(`Enter what type of measurement you desire (enter "1" for distance, or "2" for mass)...`);
-        factorMeasurement = prompt(`>> `);
-        if (parseInt(factorMeasurement) > 2 || parseInt(factorMeasurement) < 1) {
+        if (parseFloat(factorSystem) > 2 || parseFloat(factorSystem) < 1) {
             console.log(`You made a mistake.`);
         } else { break; }
     }
     while (true) {
         console.log(`Enter the value of that factor on Earth as a number.`);
         factorValue = prompt(`>> `);
-        if (isNaN(parseInt(factorValue))) {
+        if (isNaN(parseFloat(factorValue))) {
             console.log(`You made a mistake.`);
         } else { break; }
     }
-    showUserFactors(factorType, factorSystem, factorMeasurement, factorValue);
+    while (true) {
+        console.log(`Enter which galaxy you want to measure in (enter "1" for the milky way, or enter "2" for andromeda)...`);
+        factorValue = prompt(`>> `);
+        if (parseFloat(factorGalaxy) > 2 || parseFloat(factorGalaxy) < 1) {
+            console.log(`You made a mistake.`);
+        } else { break; }
+    }
+    showUserFactors(factorType, factorSystem, factorValue, factorGalaxy);
 
 }
 //Expose getUserInput globally
